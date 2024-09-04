c579f7f5d9d6c3ae21663ccc7aea1288fea9f0df
Jose Castro <jsalvadorca90@gmail.com>
2024-09-04 00:25:58 -0500

    feat: INICIO, Clase 12

---
+++ src/main/java/com/udemy/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.udemy.curso.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details() {
+        return "details";
+    }
+}
