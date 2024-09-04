3835b4ca8df521b68117f6ef3b116b507cfdd86a
Jose Castro <jsalvadorca90@gmail.com>
2024-09-04 01:37:42 -0500

    feat: Clase 13

--- src/main/java/com/udemy/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/udemy/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.udemy.curso.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title","¡Hola Mundo!");
+        model.addAttribute("name","José");
+        model.addAttribute("lastName", "Ñandú");        
         return "details";
     }
 }
