2ba9936a472fae0643e83f32dc62db72c22343f4
Jose Castro <jsalvadorca90@gmail.com>
2024-10-08 23:00:33 -0500

    feat: Clase 31 y 32

--- src/main/java/com/udemy/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/udemy/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,17 +3,37 @@ package com.udemy.curso.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.Spring;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.udemy.curso.springboot.webapp.springboot_web.models.User;
 import com.udemy.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
     
+    // La anotación @Value se usa para inyectar propiedades desde un archivo de propiedades (application.properties) directamente en un campo [atributos]
+    @Value("${config.code}")
+    private Integer code;
+    
+    @Value("${config.username}")
+    private String username;
+    
+    // @Value("${config.message}")
+    // private String message;
+    
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
     @GetMapping("/baz/{message}")
     // anotación de Spring indica que el parámetro message se va a obtener del valor que se encuentre en la URL en el lugar del placeholder {message}
     public ParamDto baz(@PathVariable String message) {
@@ -31,6 +51,28 @@ public class PathVariableController {
         json.put("product", product);
         json.put("id", id);
         // se devuelve el Map como respuesta al cliente en formato JSON
+        return json;
+    }
+
+    @PostMapping("/create")
+    // El cliente envia los datos del nuevo usuario en formato JSON en el cuerpo de la solicitud, y Spring automáticamente convierte esos datos en un objeto 'User'
+    public User create(@RequestBody User user) {
+        // Toma el nombre del usuario que se recibió en la solicitud, lo convierte a mayúsculas y lo asigna nuevamente al atributo 'name' del objeto 'user'
+        user.setName(user.getName().toUpperCase());
+        // Toma el apellido del usuario que se recibió en la solicitud, lo convierte a minúsculas y lo asigna nuevamente al atributo 'lastName' del objeto 'user'
+        user.setLastName(user.getLastName().toLowerCase());
+        // devuelve el objeto 'user' modificado como respuesta al cliente
+        return user;
+    }
+
+    @GetMapping("/values")
+    // La anotación @Values se usa para inyectar propiedades desde un archivo de propiedades (application.properties) directamente en un parámetro de un bean [argumentos]
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("code", code);
+        json.put("username", username);
+        json.put("message", message);
+        json.put("listOfValues", listOfValues);
         return json;
     }
     
